81d761fe05470917389df6ebb90f910cb7f7fe09
mosanlou <osanlou83@gmail.com>
2025-11-06 19:47:21 -0600

    Initial commit

---
+++ src/components/Shop.jsx
@@ -0,0 +1,13 @@
+
+export default function Shop({children }) {
+  return (
+    <section id="shop">
+   
+
+      <ul id="products">
+        {children}
+       
+      </ul>
+    </section>
+  );
+}
---
+++ src/dummy-products.js
@@ -0,0 +1,265 @@
+import Aquaculture from "./assets/Aquaculture.png"; 
+import Carpenter from './assets/Carpenter.png';
+import Auto_Mechanic from "./assets/Auto_Mechanic.png"; 
+import Front_Desk from "./assets/Front_Desk.png"; 
+import RN from "./assets/RN.png"; 
+import Line_Cook from "./assets/Line Cook.png"; 
+import Farm from "./assets/Farm_worker.png";
+import HVAC_Technician from "./assets/HVAC_Technician.png";
+import Heavy_Equipment_Operator from "./assets/Heavy_Equipment_Operator.png"; 
+import Medical_Lab_Technician from "./assets/Medical_Lab_Technician.png";
+import Stick_Welding from "./assets/Stick_Welding.png"; 
+import CNC from "./assets/CNC_Machine_Operator.png";
+import Sheet_metal_worker from "./assets/Sheet_metal_worker.png"; 
+import Construction_Welding from "./assets/Construction_Welding.png";
+import Continuing_Care_Aide from "./assets/Continuing_Care_Aide.png"; 
+import Hydroponics from "./assets/Hydroponics_Technician.png";
+import Steel_Production from "./assets/Steel_Production_Worker.png"; 
+import Manual_Machinist from "./assets/Manual_Machinist.png";
+import Millwright from "./assets/Millwright.png"; 
+import Mining_Engineer from "./assets/Mining_Engineer.png";
+import Mining_Machine_Operator from "./assets/Mining_Machine_Operator.png"; 
+import Pipe_Fitter from "./assets/Pipe_Fitter.png";
+import Plant_Geneticist from "./assets/Plant_Geneticist.png"; 
+import Process_Engineer from "./assets/Process_Engineer.png";
+import Quality_Assurance from "./assets/Quality_Assurance_Technician.png"; 
+import Residential_Electrician from "./assets/Residential_Electrician.png";
+import Robotic_Welding from "./assets/Robotic_Welding.png"; 
+import Robotic_Technician from "./assets/Robotic_Technician.png";
+import Room_Attendant from "./assets/Room_Attendant.png";
+
+export const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    image: Aquaculture,
+    title: 'Aquaculture',
+    price: 68000,
+    description:
+      'Aquaculture is one of the fastest growing sectors in the world, accounting for 50% of the worlds total fish production. Experience a career takes you from working indoors and monitoring fingerlings to operating technology beside a peaceful body of water.',
+  },
+  {
+    id: 'p2',
+    image: Carpenter,
+    title: 'Carpenter',
+    price: 55000,
+    description:
+      'Maneuvering through homes in various stages of construction, this experience allows you to get an idea of the range of jobs a carpenter may encounter. Constructing a header in a new build home, to installing floor – an experience not to be missed!',
+  },
+
+  {
+    id: 'p3',
+    image: Auto_Mechanic,
+    title: 'Auto Mechanic',
+    price: 80000,
+    description:
+      'As a mechanic, you will work on many different vehicles! From an older car that needs a muffler replaced, to working on disconnecting the electrical system of an electric car, this experience is diverse and highlights the variety in a mechanics day.',
+  },
+  {
+    id: 'p4',
+    image: Front_Desk,
+    title: 'Front Desk Attendant',
+    price: 50000,
+    description:
+    'A Front Desk Attendant is the primary point of contact for guests or clients, providing customer service and ensuring smooth operations at the front desk. They are the first point of contact for guests, visitors, and customers.',
+    },
+    {
+    id: 'p5',
+    image: RN,
+    title: 'Registered Nurse',
+    price: 87000,
+    description:
+    'Support patients, and interact in a virtual health care environment. Prioritize patient needs, and work to ensure that all responsibilities are taken care of before your shift ends.',
+    },
+    {
+    id: 'p6',
+    image: Line_Cook,
+    title: 'Line Cook',
+    price: 54000,
+    description:
+    'A line cook is a kitchen staff member who prepares and cooks food at a specific station in a restaurant kitchen, working under the direction of a chef.'
+    }, 
+    {
+    id: 'p7',
+    image: Farm,
+    title: 'Farm Laborer',
+    price: 56000,
+    description:
+    'Farming is becoming more and more technologically advanced! Using an automated milking machine, operating a drone for crop monitoring, and animal care, is the type of variety that makes this job an exciting part of the agricultural sector.'
+    },
+    {
+    id: 'p8',
+    image: HVAC_Technician,
+    title: 'HAVC Technician',
+    price: 100000,
+    description:
+    'Work from a metropolitan rooftop to assess the performance of heating and cooling units. They will learn to troubleshoot broken units replacing and repairing fans, wires, refrigerant levels, and complete other tasks an HVAC (Heating Ventilation and Air Conditioning) technician may face in a day on the job.',
+    },
+    {
+    id: 'p9',
+    image: Heavy_Equipment_Operator,
+    title: 'Havy Equipment Operator',
+    price: 97000,
+    description:
+    'Operate a large excavator and must help remove rubble from a trench. They will need to work multiple levers inside the cabin of a large excavator, collecting rocks using the bucket and dumping them into dump-truck.',
+    },
+    {
+    id: 'p10',
+    image: Medical_Lab_Technician,
+    title: 'Medical Lab Technician',
+    price: 86841,
+    description:
+    'Performs a variety of laboratory tests and procedures to assist in the diagnosis and treatment of diseases. They work under the supervision of a medical technologist or physician, analyzing samples like blood, and monitor the progression of conditions.'
+    }, 
+    {
+    id: 'p11',
+    image: Stick_Welding,
+    title: 'Stick Welding Technician',
+    price: 75000,
+    description:
+    'Learn about the basic components of stick welding in order to complete several welds around a construction site. They will experience working at heights on some scaffolds and will be challenged with some difficult angles, something commonly experienced by welders.'
+    }, 
+    {
+    id: 'p12',
+    image: CNC,
+    title: 'CNC Machine Operator',
+    price: 60000,
+    description:
+    'Work on a large CNC vertical milling machine. The explorer will learn to read design drafts and then program the functionality of the machine in order to fill the order.',
+    },
+    {
+    id: 'p13',
+    image: Sheet_metal_worker,
+    title: 'Sheet Metal Worker',
+    price: 60850,
+    description:
+    'Assemble and then install metal ducts in a small home workshop. The explorer will learn about manipulating sheet metal using guillotines and bending brakes to cut and bend sheet metal into the required sizes before heading over to the job site to install things.',
+    },
+    {
+    id: 'p14',
+    image: Construction_Welding,
+    title: 'Construction Welding Technician',
+    price: 54000,
+    description:
+    'Try three popular types of welding while working on the construction site of a building. The explorer will learn all about MIG, TIG, and Stick welding as they work their way up the construction site preforming a range of welding tasks.'
+    }, 
+    {
+    id: 'p15',
+    image: Continuing_Care_Aide,
+    title: 'Continuing Care Aide',
+    price: 75000,
+    description:
+    'provides ongoing care and support to individuals with disabilities or chronic illnesses in various settings, such as home health, nursing homes, or assisted living facilities. They assist with daily living activities, personal care, and provide companionship.'
+    },
+    {
+    id: 'p16',
+    image: Hydroponics,
+    title: 'Hydroponics Technician',
+    price: 59000,
+    description:
+    'Do we know how some of our tomatoes end up in the grocery store? Working in a controlled hydroponics greenhouse, you will get an idea of how they grow – from seed to table. A career that is seen as a major part of the future of food security!',
+    },
+    {
+    id: 'p17',
+    image: Steel_Production,
+    title: 'Steel Production Worker',
+    price: 79000,
+    description:
+    'Work on a large oxygen furnace which is used to produce molten steel from iron. The explorer will begin the experience in a control room overlooking the large production floor where they will prepare for the next stages of steel production.',
+    },
+    {
+    id: 'p18',
+    image: Manual_Machinist,
+    title: 'Manual Machinist',
+    price: 61000,
+    description:
+    'Learn the process of working on a large manual lathe and manual milling machine. They will be tasked with reading and interpreting design drafts, cleaning the workspace, setting up, and operating various machines.'
+    }, 
+    {
+    id: 'p19',
+    image: Millwright,
+    title: 'Millwright',
+    price: 80289,
+    description:
+    'Working in a bottling facility, you will work on executing preventative and reactive maintenance on motors; as well as working to get a production line back up and going. A millwright does a variety of tasks each day, and this experience is no exception!'
+    },
+    {
+    id: 'p20',
+    image: Mining_Engineer,
+    title: 'Mining Engineer',
+    price: 101000,
+    description:
+    'Participants experience some of the planning phases of building a mine as they learn about mine types. They will learn to pilot a drone, load blast caps and more in order to get the mine underway.',
+    },
+    {
+    id: 'p21',
+    image: Mining_Machine_Operator,
+    title: 'Mining Machine Operator',
+    price: 78000,
+    description:
+    'Learn to drive a cable shovel excavator. The explorer will use the cable shovel navigating to various sites of the mine to fill haul trucks with material from inside the mine which contains hematite rock.',
+    },
+    {
+    id: 'p22',
+    image: Pipe_Fitter,
+    title: 'Pipe Fitter',
+    price: 62970,
+    description:
+    'Learn to measure and cut large water pipes and help with the installation process. The simulation takes place at an outdoor construction site for a new hydroelectric dam. The explorer will need to read and interpret the blueprints in this simulation to cut and fit pipes to the correct length.'
+    }, 
+    {
+    id: 'p23',
+    image: Plant_Geneticist,
+    title: 'Plant Geneticist',
+    price: 84000,
+    description:
+    'Discover a career that will take you from field to laboratory, as you look to analyze and isolate genomes to optimize soy beans for growth using less environmental resources.'
+    },
+    {
+    id: 'p24',
+    image: Process_Engineer,
+    title: 'Process Engineer',
+    price: 94000,
+    description:
+    'Work in a control room to monitor and adjust the variables of four different machines. These machines include hammer crushers, magnetic separators, froth flotation cells, and Disc pelletizers. Participants must keep the production quality of iron ore pellets up.',
+    },
+    {
+    id: 'p25',
+    image: Quality_Assurance,
+    title: 'Quality Assurance Technician',
+    price: 77000,
+    description:
+    'This simulation depicts some of the activities of control, evaluation and the regulatory aspects of food processing in the large scale. Testing for bacteria, and sensory analysis of a new product is just a part of what this career entails!',
+    },
+    {
+    id: 'p26',
+    image: Residential_Electrician,
+    title: 'Residential Electrician',
+    price: 80000,
+    description:
+    'This simulation depicts some of the activities of control, evaluation and the regulatory aspects of food processing. Testing for bacteria, and sensory analysis of a new product is just a part of what this career entails!'
+    }, 
+    {
+    id: 'p27',
+    image: Robotic_Welding,
+    title: 'Robotic Welding',
+    price: 85000,
+    description:
+    'Work in a facility that manufactures parts for wind turbines. Learn to set-up and program robotic welding arms. Participants must ensure that the welding production process goes to plan.'
+    },
+    {
+    id: 'p28',
+    image: Robotic_Technician,
+    title: 'Robotic Technician',
+    price: 109000,
+    description:
+    'Perform maintenance and run diagnostics on broken robots in order to repair them. They will also need to submit performance logs and adjust line speeds as you work to ensure everything is running effectively.',
+    },
+    {
+    id: 'p29',
+    image: Room_Attendant,
+    title: 'Room Attendant',
+    price: 40000,
+    description:
+    'A room attendant, also known as a housekeeper, is a professional who cleans and maintains hotel rooms, guest areas, and other common areas in a hotel.',
+    },
+  ]; 
