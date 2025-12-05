2c76bc09ec91ce888ef7007bb31ec4fe0978a47f
Mohsen <osanlou83@gmail.com>
2025-12-05 16:13:32 -0600

    Update Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -10,7 +10,7 @@ export default function Product({ id, image, title, price, description }) {
       <div className="product-content">
         <div>
           <h3>{title}</h3>
-          <p className="product-price">Salary: ${price}</p>
+          <p className="product-price">Salary: ${price.toLocaleString()}</p>
           <p>{description}</p>
         </div>
         <p className="product-actions">
